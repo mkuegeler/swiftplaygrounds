624cdd375ef28ea7e79dbddc08f8691470e0f972
Michael Kuegeler <mkuegeler@michaelsmacbook.fritz.box>
2018-03-19 21:10:11 +0100

    Initial  commit

---
+++ MyPlayground-2018-4.playground/Sources/Slide.swift
@@ -0,0 +1,27 @@
+import Foundation
+import UIKit
+import Foundation
+import CoreGraphics
+import PlaygroundSupport
+
+class Slide {
+    func sample() -> String {
+        return "slide 1"
+    }
+    
+    func canvas () {
+        
+        // Width and height of canvas
+        let maxwidth = UIScreen.main.bounds.width
+        let maxheight = UIScreen.main.bounds.height
+        
+        // View of canvas
+        let containerView = UIView(frame: CGRect(x:0, y:0,width: maxwidth,height: maxheight))
+        
+        containerView.backgroundColor = UIColor.red
+        
+        PlaygroundPage.current.liveView = containerView
+        
+        
+    }
+}
---
+++ MyPlayground-2018-4.playground/Sources/Test2.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public class Test2 {
+    public static func run() {
+        let slide = Slide()
+        let result = slide.sample()
+        print(result)
+        
+        slide.canvas()
+        
+        
+    }
+}
