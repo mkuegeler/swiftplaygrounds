a870aec401409b0ca2e6f07165300d1477aa29a4
Michael Kuegeler <mkuegeler@michaels-macbook.fritz.box>
2018-05-22 21:27:17 +0200

    Sample code in ReadParams.swift added.

--- MyPlayground-2018-4.playground/Sources/ReadParams.swift
+++ MyPlayground-2018-4.playground/Sources/ReadParams.swift
@@ -4,6 +4,30 @@ class ReadParams {
     func do_something() -> String {
         return "Sample Class"
     }
+    
+    struct ResponseData: Decodable {
+        var person: [Person]
+    }
+    struct Person : Decodable {
+        var name: String
+        var age: String
+        var employed: String
+    }
+    
+    func loadJson(filename fileName: String) -> [Person]? {
+        if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(ResponseData.self, from: data)
+                return jsonData.person
+            } catch {
+                print("error:\(error)")
+            }
+        }
+        return nil
+    }
+    
 }
 
 
