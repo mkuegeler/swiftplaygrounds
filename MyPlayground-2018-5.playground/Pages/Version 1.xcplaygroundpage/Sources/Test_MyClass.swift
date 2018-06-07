4a6f52797d82620a2d7ee4c55af362a68feb8fc4
Michael Kuegeler <mkuegeler@michaelsmacbook.fritz.box>
2018-06-07 20:03:22 +0200

    New playground 5 with parameters in external json added

---
+++ MyPlayground-2018-5.playground/Pages/Version 1.xcplaygroundpage/Sources/Test_MyClass.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public class Test_MyClass {
+    public static func run() {
+        let my_class = MyClass()
+        let result = my_class.do_something()
+        print(result)
+    }
+}
